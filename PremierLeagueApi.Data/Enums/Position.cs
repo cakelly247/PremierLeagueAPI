698e2cf48ce9755f61b568d9b516602170bcd942
Chris Kelly <cakelly247@gmail.com>
2023-10-19 14:33:47 -0400

    Chris branch -migrations updated

---
+++ PremierLeagueApi.Data/Enums/Position.cs
@@ -0,0 +1,10 @@
+namespace PremierLeagueApi.Data.Enums
+{
+    public enum Position
+    {
+        Forward = 1,
+        Defender,
+        Midfielder,
+        GoalKeeper
+    }
+}
--- PremierLeagueApi.Data/PLDbContext.cs
+++ PremierLeagueApi.Data/PLDbContext.cs
@@ -2,14 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PremierLeagueApi.Data.Entities;
 
 namespace PremierLeagueApi.Data
 {
-    public class PLDbContext : IdentityDbContext
+    public class PLDbContext : IdentityDbContext<UserEntity, IdentityRole<int>, int>
     {
         public PLDbContext(DbContextOptions<PLDbContext> options)
             : base(options) {}
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserEntity>().ToTable("Users");
+        }
     }
 }
