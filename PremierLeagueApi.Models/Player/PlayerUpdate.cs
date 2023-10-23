aad71c1b59bbbec5c243897f65554fe4d6adf90d
Chris Kelly <cakelly247@gmail.com>
2023-10-23 14:28:20 -0400

    Manager Endpoints tested

--- PremierLeagueApi.Models/Player/PlayerUpdate.cs
+++ PremierLeagueApi.Models/Player/PlayerUpdate.cs
@@ -1,12 +1,31 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using PremierLeagueApi.Data.Enums;
 
 namespace PremierLeagueApi.Models.Player
 {
     public class PlayerUpdate
     {
-        // public int  { get; set; }
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public int JerseyNumber { get; set; }
+
+        [Required]
+        public Position Position { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [ForeignKey("Teams")]
+        public int TeamId { get; set; }
     }
 }
